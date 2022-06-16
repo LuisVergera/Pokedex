1b92547956b345227b25b3ff9cf426bb8e83e952
LuisVergera <69331928+LuisVergera@users.noreply.github.com>
2022-06-16 16:10:23 -0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 /// <reference types="jquery" />
 
-console.log($("#title"));
+let requestOptions = {
+  method: "GET",
+  redirect: "follow",
+  headers: myHeaders,
+};
+
+fetch("https://pokeapi.co/api/v2/pokemon/", requestOptions)
+  .then((respuesta) => respuesta.json())
+  .then((respuestaJSON) => {
+    Object.keys(respuestaJSON.rates).forEach((moneda) => {
+      $("ul").append($(`<li>${moneda}: ${respuestaJSON.rates[moneda]}</li>`));
+    });
+  })
+  .catch((error) => console.error("FALLÓ", error));

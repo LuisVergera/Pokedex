da31fd041e5e0a8c7063185c7bd26f3763149f73
LuisVergera <69331928+LuisVergera@users.noreply.github.com>
2022-06-16 19:01:20 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ let requestOptions = {
 fetch("https://pokeapi.co/api/v2/pokemon/", requestOptions)
   .then((respuesta) => respuesta.json())
   .then((respuestaJSON) => {
-    const pokemons = respuestaJSON["pokemon"];
-    Object.keys(pokemons).forEach((pokemon) => {
-      $("ul").append($(`<div class="card">${result.pokemons[pokemon]}</div>`));
+    const pokemons = respuestaJSON["name"];
+    Object.keys(pokemons).forEach((results) => {
+      $("ul").append($(`<div class="card">${results.pokemons[name]}</div>`));
     });
   })
   .catch((error) => console.error("FALLÓ", error));
